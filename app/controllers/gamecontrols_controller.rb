87a75bf1a2a8f643bd986e30ec6f039543d0fc6e
Chia-Tse Weng <wengyagami@hotmail.com>
2019-08-21 08:45:53 +0900

    use JS to open and close profile in Town

--- app/controllers/gamecontrols_controller.rb
+++ app/controllers/gamecontrols_controller.rb
@@ -1,21 +1,13 @@
 class GamecontrolsController < ApplicationController
   before_action :check_character
 
-  def close_profile
-    respond_to do |format|
-      format.js
-      format.html { render 'gamecontrols/town' }
-    end
-  end
-
   def wild
     @level = Level.find(params[:level])
   end
+
   private
 
   def check_character
-    if current_user.characters.empty?
-      redirect_to new_character_path
-    end
+    redirect_to new_character_path if current_user.characters.empty?
   end
 end
