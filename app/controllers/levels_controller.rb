6d61efc04220a6a6a8863951d2e4b169c22a6ef4
Chia-Tse Weng <wengyagami@hotmail.com>
2019-08-28 08:58:23 +0900

    finish basic character animation and fix some bugs

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -7,6 +7,8 @@ class LevelsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     authorize @question
+    @level = @question.level
+    @progress = @level.questions.index(@question)
     @battle_log = BattleLog.create(character: current_user.characters[0], question: @question)
     setBackground(@question)
   end
