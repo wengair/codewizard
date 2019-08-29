5910faa4c787d1d1b1225a77fa17a0425d21f3b4
Chia-Tse Weng <wengyagami@hotmail.com>
2019-08-29 21:30:39 +0900

    fix hp showing problem when purchasing the item

--- app/javascript/components/character_switch.js
+++ app/javascript/components/character_switch.js
@@ -1,16 +1,16 @@
 function char_switch() {
-  var checkboxes = document.querySelectorAll(".form-check")
+  var checkboxes = document.querySelectorAll(".form-check");
   // male checkbox
   checkboxes[0].addEventListener("click", function() {
-    document.getElementById('male').classList.remove("hidden-char")
-    document.getElementById('female').classList.add("hidden-char")
+    document.getElementById('male').classList.remove("hidden-char");
+    document.getElementById('female').classList.add("hidden-char");
   });
 
   // female checkbox
   checkboxes[1].addEventListener("click", function() {
-    document.getElementById('female').classList.remove("hidden-char")
-    document.getElementById('male').classList.add("hidden-char")
+    document.getElementById('female').classList.remove("hidden-char");
+    document.getElementById('male').classList.add("hidden-char");
   });
 };
 
-export {char_switch}
+export {char_switch};
